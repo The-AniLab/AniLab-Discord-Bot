429e9ca6550c2bbadd6701328f4ac70b49488128
harshfeudal <87577447+harshfeudal@users.noreply.github.com>
2023-07-13 23:51:26 +0700

    [Add[: language configuration
    - The language configuration will be used database to remember which user has configurated the language or not
    - The std::ifstream language is not a static variable and in the future it will be changed (default is English)

--- src/cmd_common/ping.cpp
+++ src/cmd_common/ping.cpp
@@ -1,23 +1,29 @@
+#include <fstream>
+#include <string>
+
 #include <spdlog/spdlog.h>
+#include <nlohmann/json.hpp>
 
 #include <cmd_lists.h>
+
+using json = nlohmann::json;
 
 void ping(dpp::cluster& client, const dpp::slashcommand_t& event)
 {
 	const auto ws_ping = fmt::format("`{0:.02f} ms`", event.from->websocket_ping * 1000);
 	const auto rest_ping = fmt::format("`{0:.02f} ms`", event.from->creator->rest_ping * 1000);
+	
+	json language_config;
+	std::ifstream language("../../languages/en-us.json");
 
-    const auto embed_color = client.user_get_sync(client.me.id).accent_color;
-	const auto content = fmt::format("Average Websocket Ping: {}\nRoundtrip latency: {}", ws_ping, rest_ping);
+	language >> language_config;
+	auto ping_embed = language_config["PING"];
 
 	auto create_embed = dpp::embed()
-		.set_color(embed_color)
-		.set_description(content)
-		.set_footer(
-			dpp::embed_footer()
-			.set_text(client.me.format_username())
-			.set_icon(client.me.get_avatar_url())
-		)
+		.set_color(0x9dc3f8)
+		.add_field(ping_embed["websocket"], ws_ping)
+		.add_field(ping_embed["roundtrip"], rest_ping)
+		.set_thumbnail(client.me.get_avatar_url())
 		.set_timestamp(time(0));
 
 	event.reply(dpp::message().add_embed(create_embed));
